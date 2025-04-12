6022a96cfdc77f941c9d56d2bfdb4f5963033bcc
margulies <daniel.margulies@gmail.com>
2025-04-12 09:24:29 -0500

    Update build configuration and fix dependencies

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Building production version..."
-REACT_APP_API_URL=https://smother-server.herokuapp.com/api npm run build
+REACT_APP_API_URL=https://smother-backend.herokuapp.com/api yarn build
 echo "Build completed!"
